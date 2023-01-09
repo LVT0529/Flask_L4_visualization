e7703fb2894471b26ea06ae40c9262b1fe02bd9f
LVT0529 <dlqlrxh2@naver.com>
2023-01-09 11:42:56 +0900

    LVT0529
    commit_first

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask_L4_visualization
+# L4 시각화 사이트
---
+++ myproject/myproject.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\victory\Documents\GitHub\Flask_L4_visualization\myproject
+set FLASK_APP=pybo
+set FLASK_ENV=development
+C:\Users\victory\Documents\GitHub\Flask_L4_visualization\myproject\Scripts\activate
