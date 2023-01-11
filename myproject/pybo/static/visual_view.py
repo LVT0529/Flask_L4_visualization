28b68bbec4111e0ab2a75119598e295e6617c7db
LVT0529 <dlqlrxh2@naver.com>
2023-01-11 17:33:21 +0900

    22/01/11(수) 코드

--- myproject/pybo/static/visual_view.py
+++ myproject/pybo/static/visual_view.py
@@ -1,1 +1,31 @@
-print("hello world")
+import requests
+import time
+
+
+
+try:
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(
+        url= 'https://211.49.171.10/mgmt/tm/gtm/pool/a/pool_bjapi.afgslb.afreecatv.com/members',
+        headers=headers,
+        verify=False,
+        timeout=5,
+        auth=('admin', '#ngkqns^ksG$LB')
+    )
+
+
+    print("1")
+except:
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(
+        url= 'https://211.49.171.10/mgmt/tm/gtm/pool/a/pool_bjapi.afgslb.afreecatv.com/members',
+        headers=headers,
+        verify=False,
+        timeout=5,
+        auth=('afguest', 'dk#mSLBguest')
+    )
